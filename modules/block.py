4dcab9c5a2677199882a6672b80b405db0b21caf
deox1994 <escobar.manuel@pucp.edu.pe>
2025-11-03 15:38:12 -0500

    Addition of Dual Branch Pooling with Attention Mechanism

--- modules/block.py
+++ modules/block.py
@@ -57,24 +57,32 @@ class DualBranchSPPF(nn.Module):
     """
     def __init__(self, c1, c2, k=5):
         super(DualBranchSPPF, self).__init__()
-        c_ = c1 // 2  # hidden channels
-        self.cv = Conv(c1, c_, 1, 1)
         self.k = k
-        #self.m1 = MaxPool2d(kernel_size=self.k, stride=1, padding=self.k//2)
-        self.m1 = TMaxAvgPool2d(kernel_size=self.k, stride=1, padding=self.k//2, k=3, T=0.9)
-        self.cv1 = Conv(c_ * 4, c2//2, 1, 1)
-        #self.m2 = AvgPool2d(kernel_size=self.k, stride=1, padding=self.k//2)
-        self.m2 = RWPool2d(kernel_size=self.k, stride=1, padding=self.k//2)
-        self.cv2 = Conv(c_ * 4, c2//2, 1, 1)
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 8, c2, 1, 1)
+        #self.cv1 = Conv(c_ * 4, c2//2, 1, 1)
+        #self.cv2 = Conv(c_ * 4, c2//2, 1, 1)
+        
+        """ Pooling Types """
+        self.m1 = MaxPool2d(kernel_size=self.k, stride=1, padding=self.k//2)
+        #self.m1 = TMaxAvgPool2d(kernel_size=self.k, stride=1, padding=self.k//2, k=3, T=0.9)
+        self.m2 = AvgPool2d(kernel_size=self.k, stride=1, padding=self.k//2)
+        #self.m2 = RWPool2d(kernel_size=self.k, stride=1, padding=self.k//2)
+        
+        """ Attention Mechanism """
+        #self.att = CBAM(c_ * 4)
+        self.att = LSKA(c_ * 8)
 
     def forward(self, x):
-        x_aux = self.cv(x)
+        x_aux = self.cv1(x)
         y1 = [x_aux]
         y2 = [x_aux]
         y1.extend(self.m1(y1[-1]) for _ in range(3))
         y2.extend(self.m2(y2[-1]) for _ in range(3))
-        return torch.cat([self.cv1(torch.cat(y1, 1)), self.cv2(torch.cat(y2, 1))], 1)
-    
+        return self.cv2(self.att(torch.cat([torch.cat(y1, 1), torch.cat(y2, 1)], 1)))
+        
+        
 class Bottleneck_ScConv(Bottleneck):
     def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
         super().__init__(c1, c2, shortcut, g, k, e)
